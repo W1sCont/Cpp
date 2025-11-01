4ce4f9e89bc2c60d5720401514553e36102dc4d4
W1sCont <tmrostik@gmail.com>
2025-11-01 20:04:06 +0100

    lesson2

---
+++ lesson1/lesson1/lesson1.cpp
@@ -0,0 +1,61 @@
+﻿//#include <iostream>
+//
+//int main()
+//{
+	//std::cout << "\t\"Hello, world!\"\n"; // строковий коментар
+	///*блочний коментар
+	//\t tab
+	//\знак для виводу
+	//*/
+	//std::cout << "C++\n";
+
+	// //task1
+	//std::cout << "C\n+\n+\n";
+
+	// task2
+	/*std::cout << "\"Hello, world!\"\n";
+	std::cout << "\/ Bjarne Stroustrup\/";*/
+
+	/*int valueInt = 10000;
+	double valueDouble = 10.5;
+	char valueChar = 'A';
+	bool valueBool = true;
+
+	std::string str = "hello world";
+	std::string str2;
+
+	int x = valueInt;
+	int y = valueDouble;
+
+	str2 = "Cat";
+
+	char value = 65;
+
+	std::cout << "valueInt: " << valueInt << std::endl;
+	std::cout << "valueDouble: " << valueDouble << std::endl;
+	std::cout << "valueChar: " << valueChar << std::endl;
+	std::cout << "valueBool: " << valueBool << std::endl;
+	std::cout << "str: " << str << std::endl;
+	std::cout << "x: " << x << std::endl;
+	std::cout << "str2: " << str2 << std::endl;
+	std::cout << "y: " << y << std::endl;
+	std::cout << "value: " << value << std::endl;*/
+
+
+//	// task3
+//	int aquariumDavid = 8;
+//	int aquariumAlex = 11;
+//
+//	std::cout << "aquariumAlex: " << aquariumAlex << std::endl;
+//	std::cout << "aquariumDavid: " << aquariumDavid << std::endl;
+//
+//	int memory = aquariumAlex;
+//
+//	aquariumAlex = aquariumDavid;
+//
+//	aquariumDavid = memory;
+//
+//	std::cout << "aquariumAlex: " << aquariumAlex << std::endl;
+//	std::cout << "aquariumDavid: " << aquariumDavid << std::endl;
+//
+//}
---
+++ lesson1/lesson1/lesson2.cpp
@@ -0,0 +1,130 @@
+#include <iostream>
+
+int main() {
+
+	/*std::cout << "Hello, world!" << std::endl;
+	int vInt = 10;
+	bool vBool = true;
+	char vChar = 'A';
+	double vDouble = 10.5;
+
+	std::cout << vInt << std::endl;*/
+
+	/*int age;
+
+	std::cout << "Enter your age: ";
+	std::cin >> age;
+
+	std::cout << age << std::endl;*/
+
+	/*char value, valueTwo;
+	std::cout << "Enter value: ";
+	std::cin >> value >> valueTwo;
+	std::cout << value << " " << valueTwo << std::endl;*/
+
+	// task 1
+
+	/*int valueInt;
+	double valueDouble;
+	char  valueChar;
+	std::string valueString;
+
+	std::cout << "Enter int: " << std::endl;
+	std::cin >> valueInt;
+	
+	std::cout << "Enter double: " << std::endl;
+	std::cin >> valueDouble;
+
+	std::cout << "Enter cher: " << std::endl;
+	std::cin >> valueChar;
+
+	std::cout << "Enter string:  " << std::endl;
+	std::cin >> valueString;
+
+
+	std::cout << "You enter:" << valueInt << "," << valueDouble << "," << valueChar << "," << valueString;*/
+
+
+	/*int x = 10, y = 20;*/
+	/*x = x + 5;
+	std::cout << x << std::endl;
+
+	y += 12;
+	std::cout << y << std::endl;
+
+	x *= 5;
+	std::cout << x << std::endl;
+
+	std::cout << x + y << std::endl;
+	std::cout << x - y << std::endl;
+	std::cout << x * y << std::endl;
+	std::cout << x / y << std::endl;*/
+
+
+	// increment ++
+	// decrement --
+	/*int x = 10, y = 20;*/
+	/*y = y - 1;
+	std::cout << y << std::endl;
+	y--;
+	std::cout << y << std::endl;
+
+	x = x + 1;
+	std::cout << x << std::endl;
+	x++;
+	std::cout << x << std::endl;*/
+
+
+	// ++y y++
+	
+	/*std::cout << y++ << std::endl;
+	std::cout << x++ << std::endl;
+
+	std::cout << ++y << std::endl;
+	std::cout << ++x << std::endl;*/
+
+
+	// task 2
+	/*int int1;
+
+	std::cout << "Enter crabs: ";
+	std::cin >> int1;
+
+	int result;
+	result = int1 / 3;
+	std::cout << "You can created: " << result << " Trio" << std::endl;*/
+
+	//int x = 10;
+	//int y = 20;
+
+	//std::cout << (x == y) << std::endl; // 0
+	//std::cout << (x != y) << std::endl; // 1
+	//std::cout << (x > y) << std::endl; // 0
+	//std::cout << (x < y) << std::endl; // 1
+
+	//bool value = (x != y);
+	//std::cout << value << std::endl;
+
+	//char vChar = 'A';
+	//std::cout << (int)vChar << std::endl;
+
+
+	// task3 
+	
+	/*int value1, value2;
+	std::cout << "Enter number 1: ";
+	std::cin >> value1;
+
+	std::cout << "Enter number 2: ";
+	std::cin >> value2;
+
+	bool result;
+	result = value1 == value2;
+	std::cout << result;*/
+
+	
+
+
+
+
+}
