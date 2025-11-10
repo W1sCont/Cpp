594b2b7fcf79420b84a9dba2468ac3460804c7bd
W1sCont <tmrostik@gmail.com>
2025-11-10 20:18:57 +0100

    lesson 4

---
+++ lesson1/lesson1/lesson4.cpp
@@ -0,0 +1,118 @@
+//#include <iostream>
+//
+//int main() {
+//
+//	// task1 
+//	/*int number;
+//	std::cout << "Enter number: ";
+//	std::cin >> number;*/
+//
+//	/*if (number % 2 == 0) {
+//		number *= 3;
+//	}
+//	else {
+//		number /= 2;
+//	}
+//	std::cout << "result " << number << std::endl;*/
+//
+//	/*std::cout << "result " <<((number % 2 == 0) ? (number *= 3) : (number /= 2)) << std::endl;*/
+//
+//
+//	// task 2
+//
+//	/*int num1, num2;
+//	std::cout << "Enter number 1: ";
+//	std::cin >> num1;
+//	std::cout << "Enter number 2: ";
+//	std::cin >> num2;
+//
+//	if (num1 == num2) {
+//		std::cout << "number1 == number2" << std::endl;
+//	}
+//	else if (num1 > num2){
+//		std::cout << num2 << ", " << num1 << std::endl;
+//	}
+//	else
+//		std::cout << num1 << ", " << num2 << std::endl;*/
+//
+//	//SWITCH
+//
+//	/*char value;
+//	std::cout << "Enter number: ";
+//	std::cin >> value;
+//
+//	enum WEEKS {
+//		mon = 1,
+//		tues,
+//		wed,
+//		thur,
+//		frid,
+//		sayur,
+//		sund
+//	};
+//
+//
+//	switch (value) {
+//	case WEEKS::mon:
+//		std::cout << "Monday" << std::endl;
+//		break;
+//	case '2':
+//		std::cout << "Tuesday" << std::endl;
+//		break;
+//	default:
+//		std::cout << "Else";*/
+//
+//
+//	// task 3
+//
+//	/*int num;
+//	std::cout << "Enter number: ";
+//	std::cin >> num;
+//
+//	switch (num) {
+//	case 1:
+//		std::cout << "Latte" << std::endl;
+//		break;
+//	case 2:
+//		std::cout << "Americano" << std::endl;
+//		break;
+//	case 3:
+//		std::cout << "Macchiato" << std::endl;
+//		break;
+//	case 4:
+//		std::cout << "Capuccino" << std::endl;
+//		break;
+//	case 5:
+//		std::cout << "Espresso" << std::endl;
+//		break;
+//	default:
+//		std::cout << "Not in list" << std::endl;
+//	}*/
+//
+//
+//	// while
+//
+//	//int num = 1;
+//	//while (num < 6) {
+//	//	std::cout << "Number " << num++ << std::endl;
+//	//	/*num++;*/
+//	//}
+//
+//
+//	// task 4
+//
+//	/*int num, sum = 0;
+//	std::cout << "Enter num for 1 to 500: ";
+//	std::cin >> num;
+//
+//	if (num <= 500 && num >0) {
+//		while (num <= 500) {
+//			sum += num++;
+//		}
+//	}
+//
+//	std::cout << sum << std::endl;*/
+//
+//
+//	return 0;
+//}
