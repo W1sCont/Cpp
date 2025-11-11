59a1807c972218a294b36dbf4b825ae6a2f35271
W1sCont <tmrostik@gmail.com>
2025-11-11 22:12:27 +0100

    Code 1, 2
    task from CodeWars 1 , 2

---
+++ lesson1/lesson1/code1.cpp
@@ -0,0 +1,20 @@
+//#include <iostream>
+//
+//
+////Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+//
+//
+//std::string even_or_odd(int number) {
+//
+//	return (number % 2 == 0) ? "Even" : "Odd";
+//}
+//
+//
+//int main() {
+//
+//	even_or_odd(10);
+//	std::cout << even_or_odd(10) << std::endl;
+//
+//
+//	return 0;
+//}
---
+++ lesson1/lesson1/code2.cpp
@@ -0,0 +1,25 @@
+//#include <iostream>
+//#include <string>
+//
+////We need a function that can transform a string into a number.What ways of achieving this do you know ?
+////
+////Note : Don't worry, all inputs will be strings, and every string is a perfectly valid representation of an integral number.
+//
+////"1234" -- > 1234
+////"605"  -- > 605
+////"1405" -- > 1405
+////"-7" -- > -7
+//
+//int string_to_number(const std::string& s) {
+//	return std::stoi(s);
+//}
+//
+//
+//int main() {
+//
+//	std::string num_str = "1405";
+//	int num = string_to_number(num_str);
+//	std::cout << num << std::endl;
+//
+//	return 0;
+//}
