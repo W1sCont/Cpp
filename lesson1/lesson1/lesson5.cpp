cdffad34cc7f92706c3d23cfa82408733a2f178c
W1sCont <tmrostik@gmail.com>
2025-11-12 20:16:30 +0100

    lesson 5

---
+++ lesson1/lesson1/lesson5.cpp
@@ -0,0 +1,84 @@
+//#include <iostream>
+//
+//int main() {
+//
+//	// task 1
+//	/*int temperature;
+//	do {
+//		std::cout << "Enter temperature: ";
+//		std::cin >> temperature;
+//	} while (temperature < 60);
+//
+//	std::cout << "Fire!!!" << std::endl;*/
+//
+//	// task 2
+//	/*int num;
+//
+//	std::cout << "Enter seconds: ";
+//	std::cin >> num;
+//
+//	if (num > 0 && num <= 60) {
+//		while (num-- > 0) {
+//			std::cout << num << " sec" << std::endl;
+//		}
+//	}*/
+//
+//	// task 3
+//	/*int num, result{};
+//
+//	do {
+//		std::cout << "Enter number: ";
+//		std::cin >> num;
+//		result += num;
+//	} while (num > 0);
+//
+//	std::cout << "Result: " << result << std::endl;*/
+//
+//
+//	// task 4
+//
+//	/*for (int x = 0; x <= 20; x++) {
+//		if (x % 2 == 0) {
+//			std::cout << x << std::endl;
+//		}
+//	
+//	}*/
+//	
+//	// task 5
+//	/*int result{};
+//	for (int x = 0; x <= 1000; x++) {
+//		result += x;
+//	}
+//	std::cout << result / 1000 << std::endl;*/
+//
+//	// task 6
+//
+//	/*int sklad, autoS, result{};
+//
+//	std::cout << "Enter how many apples are in stock: ";
+//	std::cin >> sklad;
+//	std::cout << "Enter how many apples the truck can carry: ";
+//	std::cin >> autoS;
+//
+//	while (sklad >= 0) {
+//		sklad -= autoS;
+//		result++;
+//	}
+//	std::cout << result << std::endl;*/
+//
+//
+//	// for each
+//	/*int arr[10] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+//
+//	for (int element : arr) {
+//		std::cout << element << ' ';
+//	}
+//
+//	std::cout << std::endl;*/
+//
+//
+//
+//
+//
+//	return 0;
+//}
